9217f2947394c9452c5404fb9f0dd0bf2b1290bf
Aline <aline_sinclaire@hotmail.fr>
2020-09-20 23:09:28 +0200

    buttons in the NavBar finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,22 +6,25 @@ import { Link } from 'react-router-dom';
 
 
 function NavBar() {
-
+      const navbar = { color: 'grey', fontSize: '30px', fontFamily:'serif', borderStyle: 'double'  }
+      const navbar2 = { marginLeft: '75%', color: 'grey', fontSize: '30px', fontFamily:'serif', borderStyle: 'double' }
       return (
             <Navbar scrolling dark expand="md" fixed="top" >
 
                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
                   <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="container-fluid" >
+
                               <Nav.Link
                                     as={Link}
                                     to="/" >HOME</Nav.Link>
 
                               <Nav.Link
-                              style={{ marginLeft: '1000px' }}
+
+                                    style={navbar2}
                                     as={Link}
                                     to="/lessons">ALL LESSONS</Nav.Link>
-                              <Nav.Link className="ml-auto" as={Link} to="/login">LOGIN</Nav.Link>
+                              <Nav.Link className="ml-auto" as={Link} to="/login" style={navbar}>LOGIN</Nav.Link>
 
                         </Nav>
 

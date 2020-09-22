0b91476449d529ed6f5110b34dd978a72313dac2
Aline <aline_sinclaire@hotmail.fr>
2020-09-22 18:07:56 +0200

    buttons are rounded and ok but still at left side

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,17 +9,18 @@ function NavBar() {
 
     return (
 
-        <Navbar inverse fluid scrolling light fixed="top" >
+        <Navbar fluid scrolling light fixed="top" >
             <Navbar.Brand href="https://www.celebrityschool.in/" >
-                <img 
-                alt="Cschool logo"
-                src={NavbarLogo}
+                <img
+                    alt="Cschool logo"
+                    src={NavbarLogo}
                 />
             </Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="https://www.celebrityschool.in/online-all-classes" > ALL LESSONS </Nav.Link>
+                <Nav.Link style={{ color: 'whitesmoke', borderStyle: 'double', borderRadius: '25px' }} href="https://www.celebrityschool.in/online-all-classes" > ALL LESSONS </Nav.Link>
 
-                <Nav.Link href="" > LOGIN </Nav.Link>
+                <Nav.Link style={{
+                    color: 'whitesmoke', borderStyle: 'double', borderRadius: '25px', marginLeft: '1.8rem' }} className="links2" href="" > LOGIN </Nav.Link>
             </Nav>
 
         </Navbar>

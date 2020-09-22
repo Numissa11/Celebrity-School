27577dc50499f00fc7fa58ade8d26f75bf96f3e0
Shekhar Mishra <shekharmishra.krish@gmail.com>
2020-09-22 23:21:19 +0530

    added style for right aligin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav,Row,Col } from 'react-bootstrap'
 import './Navbar.css';
 import NavbarLogo from '../images/logo.png'
 
@@ -10,17 +10,25 @@ function NavBar() {
     return (
 
         <Navbar inverse fluid scrolling light fixed="top" >
-            <Navbar.Brand href="https://www.celebrityschool.in/" >
+         <Row style={{width:'100%'}}>
+             <Col md={4}>
+             <Navbar.Brand href="https://www.celebrityschool.in/" >
                 <img 
                 alt="Cschool logo"
                 src={NavbarLogo}
                 />
             </Navbar.Brand>
-            <Nav className="mr-auto">
+             </Col>
+             <Col md={4}></Col>
+             <Col md={4}>
+             <Nav className="mr-auto">
                 <Nav.Link href="https://www.celebrityschool.in/online-all-classes" > ALL LESSONS </Nav.Link>
 
                 <Nav.Link href="" > LOGIN </Nav.Link>
             </Nav>
+             </Col>
+         </Row>
+           
 
         </Navbar>
 

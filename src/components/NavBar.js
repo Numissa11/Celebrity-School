637977ffe65d13115be0990ff251b7fc08e3df1d
Aline <aline_sinclaire@hotmail.fr>
2020-09-26 00:52:44 +0200

    no Bootstrap navbar not finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Navbar, Nav, Row, Col, Modal, } from 'react-bootstrap';
+import {Modal, } from 'react-bootstrap';
 import './Navbar.css';
 import NavbarLogo from '../images/logo.png'
 import Login from './Login';
 
+
+/*  <img
+          alt="Productized logo"
+          src={NavbarLogo}
+          width="140"
+          height="44"
+          className="d-inline-block align-top logo"
+        />*/
+
 class NavBar extends React.Component {
     constructor() {
         super()
-        
+
         this.handleShow = this.handleShow.bind(this)
     }
+    
 
 
     state = { isSignedIn: false, show: false }
@@ -17,7 +27,7 @@ class NavBar extends React.Component {
     handleShow() {
         this.setState({
             show: true
-        })
+        }) 
     }
     // MODAL CLOSE FUNCTION
     handleClose() {
@@ -29,30 +39,39 @@ class NavBar extends React.Component {
     render() {
         return (
 
-            <div>
+     <div> 
 
-                <Navbar inverse fluid scrolling light fixed="top" >
-                    <Row style={{ width: '100%' }}>
-                        <Col md={4}>
-                            <Navbar.Brand href="https://www.celebrityschool.in/" >
-                                <img
-                                    alt="Cschool logo"
-                                    src={NavbarLogo}
-                                />
-                            </Navbar.Brand>
-                        </Col>
-                        <Col md={4}></Col>
-                        <Col md={4}>
-                            <Nav className="mr-auto">
-                                <Nav.Link style={{ borderStyle: 'solid', color: 'whitesmoke', borderRadius: '25px', marginLeft: '1.8rem' }} href="https://www.celebrityschool.in/online-all-classes" > ALL LESSONS </Nav.Link>
+                         {/* NAVBAR */}
 
-                                <Nav.Link style={{ borderStyle: 'solid', color: 'whitesmoke', borderRadius: '25px', marginLeft: '1.8rem' }} className="links2" onClick={this.handleShow.bind(this)} > LOGIN </Nav.Link>
-                            </Nav>
-                        </Col>
-                    </Row>
+<header className='toolbar'>
+    <nav className='toolbar_navigation'>
+        <div></div>
 
+        <div className='toolbar_logo'>
+            <a href='www.celebrityschool.in'>
+        <img
+          alt="Productized logo"
+          src={NavbarLogo}
+          width="140"
+          height="44"
+          className="d-inline-block align-top logo"
+        />
+        </a>
+        </div>
+        <div className="spacer"></div>
+        <div className='toolbar_navigation_items'> 
+            <ul>
+                <li><a href='/login'>LOGIN</a></li>
+                <li><a href='lessons'>ALL LESSONS</a></li>
 
-                </Navbar>
+            </ul>
+        </div>
+    </nav>
+</header>
+    
+
+                         {/* MODAL */}
+
                 <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
                     <Modal.Header closeButton>
                         <Modal.Title>   </Modal.Title>
